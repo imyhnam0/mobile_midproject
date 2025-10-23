63459a982926d7f1eb2d1a7eaea074f49a197558
imyhnam0 <yoonhyung9775@gmail.com>
2025-10-23 19:19:59 +0900

    blog add

--- PhotoBlogServer/blog/views.py
+++ PhotoBlogServer/blog/views.py
@@ -1,9 +1,12 @@
-from rest_framework import viewsets
+from django.shortcuts import render
 from .models import Post
+from rest_framework import viewsets
 from .serializers import PostSerializer
-
 
 class BlogImages(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
--- PhotoBlogServer/mysite/urls.py
+++ PhotoBlogServer/mysite/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('', views.post_list, name='post_list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
